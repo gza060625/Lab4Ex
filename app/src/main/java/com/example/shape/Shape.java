f946eba117ec4bddee76b0437330e41173dcac1b
gza060625 <gza060625@gmail.com>
2019-10-03 19:38:34 -0600

    Update Shape.java

--- app/src/main/java/com/example/shape/Shape.java
+++ app/src/main/java/com/example/shape/Shape.java
@@ -3,7 +3,7 @@ package com.example.shape;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    private String color="$SOmeColor";
+    private String color="$Somesomecolor";
 
     
 
